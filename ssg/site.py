615fd58101861b96d2af5417ba27b1b447224087
Halim-Mouaziz <89255951+Halim-Mouaziz@users.noreply.github.com>
2021-08-24 14:51:16 +0100

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -4,16 +4,15 @@ import os
 class Site:
 
     def __init__(self, source, dest):
-        self.source = source.Path()
-        self.dest = dest.Path()
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest / self.source.relative_to()
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents = True, exist_ok = True)
 
     def build(self):
         self.dest.mkdir(parents = True, exist_ok = True)
         for path in self.source.rglob("*"):
-            isDirectory = os.path.isdir(path)
-            if isDirectory is True:
-                path.create_dir(path)
+            if path.is_dir():
+                self.create_dir(path)
