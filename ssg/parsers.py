2a163899a981bf7328c9a7a36878192ee9921d7c
Halim-Mouaziz <89255951+Halim-Mouaziz@users.noreply.github.com>
2021-08-25 14:47:16 +0100

    completed

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -25,3 +25,9 @@ class Parser:
     def copy(self, path, source, dest):
         path.copy2(path, dest / path.relative_to(source))
 
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+
+    def parse(self, path : Path, source : Path , dest : Path):
+        Parser.copy(path, source, dest)
