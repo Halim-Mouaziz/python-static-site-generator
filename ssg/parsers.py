73446c61577ceaa426a0e61745cd436ae3b6737d
Halim-Mouaziz <89255951+Halim-Mouaziz@users.noreply.github.com>
2021-08-26 13:49:08 +0100

    Completed

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -43,7 +43,7 @@ class MarkdownParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        html.self.write(path, dest)
+        self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
@@ -53,6 +53,6 @@ class ReStructuredTextParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html15")
-        html["html_body"].self.write(path, dest)
+        self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
